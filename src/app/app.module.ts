011a34228f026164a656d7fc861f74103c8d9879
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-07-20 13:08:55 +0300

    (add) service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { StatusBar } from "@ionic-native/status-bar";
 
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
+import { EmployeeProvider } from "../providers/employee/employee";
 
 @NgModule({
   declarations: [MyApp, HomePage],
@@ -16,6 +17,7 @@ import { HomePage } from "../pages/home/home";
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
+    EmployeeProvider,
   ],
 })
 export class AppModule {}
